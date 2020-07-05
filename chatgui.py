eec30a78fa24ae3851bea3c2a546cebd37f4f9a6
Monespiseth Ly <piseth@MacBook-Pro.local>
2020-07-05 12:17:21 +0700

    change to cli

--- chatgui.py
+++ chatgui.py
@@ -75,50 +75,63 @@ def chatbot_response(msg):
 # Creating GUI with tkinter
 
 
-def send():
-    msg = EntryBox.get("1.0", 'end-1c').strip()
-    EntryBox.delete("0.0", END)
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("Fou: ")
+        if inp.lower() == "quit":
+            break
 
-    if msg != '':
-        ChatLog.config(state=NORMAL)
-        ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#442265", font=("Times New Roman", 12))
+        results = chatbot_response(inp)
 
-        res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
+        print('Su Ling (A.I): ' + results)
 
-        ChatLog.config(state=DISABLED)
-        ChatLog.yview(END)
+# def send():
+#     msg = EntryBox.get("1.0", 'end-1c').strip()
+#     EntryBox.delete("0.0", END)
+
+#     if msg != '':
+#         ChatLog.config(state=NORMAL)
+#         ChatLog.insert(END, "You: " + msg + '\n\n')
+#         ChatLog.config(foreground="#442265", font=("Times New Roman", 12))
+
+#         res = chatbot_response(msg)
+#         ChatLog.insert(END, "Bot: " + res + '\n\n')
+
+#         ChatLog.config(state=DISABLED)
+#         ChatLog.yview(END)
 
 
-base = Tk()
-base.title("Hello")
-base.geometry("400x500")
-base.resizable(width=FALSE, height=FALSE)
+chat()
+
+# base = Tk()
+# base.title("Hello")
+# base.geometry("400x500")
+# base.resizable(width=FALSE, height=FALSE)
 
 # Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+# ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
 
-ChatLog.config(state=DISABLED)
+# ChatLog.config(state=DISABLED)
 
 # Bind scrollbar to Chat window
-scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
-ChatLog['yscrollcommand'] = scrollbar.set
+# scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
+# ChatLog['yscrollcommand'] = scrollbar.set
 
 # Create Button to send message
-SendButton = Button(base, font=("Times New Roman", 12, 'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b", fg='#ffffff',
-                    command=send)
+# SendButton = Button(base, font=("Times New Roman", 12, 'bold'), text="Send", width="12", height=5,
+#                     bd=0, bg="#32de97", activebackground="#3c9d9b", fg='#ffffff',
+#                     command=send)
 
-# Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
-#EntryBox.bind("<Return>", send)
+# # Create the box to enter message
+# EntryBox = Text(base, bd=0, bg="white", width="29", height="5", font="Arial")
+# #EntryBox.bind("<Return>", send)
 
 
-# Place all components on the screen
-scrollbar.place(x=376, y=6, height=386)
-ChatLog.place(x=6, y=6, height=386, width=370)
-EntryBox.place(x=128, y=401, height=90, width=265)
-SendButton.place(x=6, y=401, height=90)
+# # Place all components on the screen
+# scrollbar.place(x=376, y=6, height=386)
+# ChatLog.place(x=6, y=6, height=386, width=370)
+# EntryBox.place(x=128, y=401, height=90, width=265)
+# SendButton.place(x=6, y=401, height=90)
 
-base.mainloop()
+# base.mainloop()
